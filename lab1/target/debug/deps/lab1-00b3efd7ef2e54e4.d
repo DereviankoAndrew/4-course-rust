f451eb877a2fa9a31ebeaadbbbff2238ebba34bf
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2024-10-16 12:57:58 +0200

    lab1

---
+++ lab1/target/debug/deps/lab1-00b3efd7ef2e54e4.d
@@ -0,0 +1,5 @@
+/Users/andriiderevianko/Documents/GitHub/4-course-rust/lab1/target/debug/deps/lab1-00b3efd7ef2e54e4: src/main.rs
+
+/Users/andriiderevianko/Documents/GitHub/4-course-rust/lab1/target/debug/deps/lab1-00b3efd7ef2e54e4.d: src/main.rs
+
+src/main.rs:
---
+++ lab1/target/debug/lab1.d
@@ -0,0 +1,1 @@
+/Users/andriiderevianko/Documents/GitHub/4-course-rust/lab1/target/debug/lab1: /Users/andriiderevianko/Documents/GitHub/4-course-rust/lab1/src/main.rs
